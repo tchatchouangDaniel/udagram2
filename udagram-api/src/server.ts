7f46f5ee8f7eec33c003db7a8d6543d7e6fe7bc8
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-09-23 22:50:28 +0200

    update api

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -22,7 +22,14 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(express.json());
 
-  app.use(cors());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
   app.use("/api/v0/", IndexRouter);
 
